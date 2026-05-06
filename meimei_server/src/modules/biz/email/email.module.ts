90ef3571d8f4e751009d0cc966aebeb7508d6aac
game001 <game001@game.com>
2026-05-06 14:28:45 +0800

    d

--- meimei_server/src/modules/biz/email/email.module.ts
+++ meimei_server/src/modules/biz/email/email.module.ts
@@ -3,14 +3,18 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from '../order/entities/order.entity';
 import { OrderItem } from '../order/entities/order-item.entity';
+import { OrderModule } from '../order/order.module';
 import { EmailService } from './email.service';
+import { EmailController } from './email.controller';
 
 @Global()
 @Module({
   imports: [
     ConfigModule,
     TypeOrmModule.forFeature([Order, OrderItem]),
+    OrderModule,
   ],
+  controllers: [EmailController],
   providers: [EmailService],
   exports: [EmailService],
 })
